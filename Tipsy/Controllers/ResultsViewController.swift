dcb29f01696aed635b67cb37b2d73b52cd51705d
besimsherifi <sherifibesim@gmail.com>
2022-04-24 19:16:46 +0200

    created the view controllers and linked the iboutlets

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by besim on 24.4.22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func recalculate(_ sender: UIButton) {
+    }
+    
+
+}
